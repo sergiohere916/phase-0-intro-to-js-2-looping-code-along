e3c50903bbc6fe4ae8634591e914b0d091aae4e6
Sergio Heredia <sergioheredia1996@gmail.com>
2023-07-16 15:24:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(listOfNames, event) {
+    let eventMessages = [];
+    for (let i=0; i < listOfNames.length; i++) {
+        let currentName = listOfNames[i];
+        let message = `Thank you, ${currentName}, for the wonderful ${event} gift!`;
+        eventMessages.push(message);
+    }
+    return eventMessages;
+}
+function countDown(number = 15) {
+    while (number >= 0) {
+        console.log(number);
+        number --;
+    }
+}
